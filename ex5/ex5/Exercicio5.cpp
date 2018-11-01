d997c0ea4e00172162aefe3feeef428535f22e4c
SerBasti <sbstcosta@gmail.com>
2018-11-01 20:38:21 +0000

    BIG chnages

--- ex5/ex5/Exercicio5.cpp
+++ ex5/ex5/Exercicio5.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<Windows.h>
 #include<tchar.h>
+#include "Exercicio5.h"
 
 #define INTEL 0x4949
 #define MOTOROLA 0x4d4d
@@ -21,7 +22,7 @@
 #define IMAGE_WIDTH_INTEL 0X02a0
 
 #define IMAGE_HEIGHT_MOT 0Xa003
-#define IMAGE_HEIGHT_INTEL 0Xa003
+#define IMAGE_HEIGHT_INTEL 0X03a0
 
 
 
@@ -40,55 +41,116 @@ typedef struct {
 }TIFF_HEADER, *PTIFF_HEADER;
 
 typedef struct {
-	USHORT numberOfEntries;
-}NUMBER_ENTRIES, *PNUMBER_ENTRIES;
-
-typedef struct {
 	USHORT tagNumber;
 	USHORT dataFormat;
 	UINT nrOfComponents;
 	UINT offsetDataValue;
 }ENTRIES, *PENTRIES;
 
+
+typedef struct {
+	USHORT numberOfEntries;
+	PENTRIES entries[1];
+}NUMBER_ENTRIES, *PNUMBER_ENTRIES;
+
+
+
 #pragma pack(pop)
 
+DWORD intelij[2] = {INTEL,MOTOROLA};
+
+CHAR data_formats[12] = {'%d','%s','%d','%d','%f','%d',NULL,'%d','%d','%f','%f','%f'};
 
 
 VOID PrintLastError() {
 	printf("" + GetLastError());
 }
 
-VOID VerifyInIntelMode(PENTRIES entry) {
-
+VOID VerifyInIntelMode(PENTRIES entry,PTIFF_HEADER header ) {
+	char *data_address = (char *)(header + entry->offsetDataValue);
+	char data_format = data_formats[entry->dataFormat];
+	
+	if (entry->tagNumber == MODEL_INTEL) {
+		printf("Model: " + data_format, *data_address);
+		return;
+	}		
+	if (entry->tagNumber == DATE_TIME_INTEL) {
+		printf("Data: " + data_format, *data_address);
+		return;
+	}
+	if (entry->tagNumber == ISO_INTEL) {
+		printf("ISO: " + data_format, *data_address);
+		return;
+	}
+	if (entry->tagNumber == APERTURE_VALUE_INTEL){
+		printf("Abertura: F 1 / " + data_format, *data_address);
+		return;
+	}		
+	if (entry->tagNumber == IMAGE_WIDTH_INTEL) {
+		printf("Dimensão: " + data_format, *data_address);
+		printf(" px x ");
+		return;
+	}		
+	if (entry->tagNumber == IMAGE_HEIGHT_INTEL) {
+		printf(data_format + " px", *data_address);
+	}
 }
 
-VOID VerifyInMMMode(PENTRIES entry) {
- 
+VOID VerifyInMMMode(PENTRIES entry, PTIFF_HEADER header) {
+	char *data_address = (char *)(header + entry->offsetDataValue);
+	char data_format = data_formats[entry->dataFormat];
+
+	if (entry->tagNumber == MODEL_MOT){
+		printf("Model: " + data_format, *data_address);
+		return;
+	}
+	if (entry->tagNumber == DATE_TIME_MOT) {
+		printf("Data: " + data_format, *data_address);
+		return;
+	}
+		
+
+	if (entry->tagNumber == ISO_MOT) {
+		printf("ISO: " + data_format, *data_address);
+		return;
+	}
+		
+	if (entry->tagNumber == APERTURE_VALUE_MOT) {
+		printf("Abertura: F 1 / " + data_format, *data_address);
+		return;
+	}
+	if (entry->tagNumber == IMAGE_WIDTH_MOT) {
+		printf("Dimensão: " + data_format, *data_address);
+		printf(" px x ");
+		return;
+	}
+	if (entry->tagNumber == IMAGE_HEIGHT_MOT)
+		printf(data_format + " px", *data_address);
 }
 
 //Navigate through pointers in the structure and print the metadata
 VOID PrintTags(LPVOID baseView) {
 	PTIFF_HEADER tHeader = (PTIFF_HEADER)baseView;
-	while (tHeader->mark != 0xFFE1){
+
+	while ((char*)tHeader == (char*)EOF && tHeader->mark != 0xFFE1){
 		baseView = (char *)((char *)baseView + tHeader->size + sizeof(tHeader ->mark));
 		tHeader = (PTIFF_HEADER)baseView;
 	}
-	PNUMBER_ENTRIES nrOfEntries = (PNUMBER_ENTRIES)(tHeader->offsetToFirstIFD + tHeader);
-	PENTRIES entry = (PENTRIES)(nrOfEntries + sizeof(PNUMBER_ENTRIES));
-	for (DWORD i = 0; i < nrOfEntries->numberOfEntries; ++i) {
+	PNUMBER_ENTRIES entries = (PNUMBER_ENTRIES)(tHeader->offsetToFirstIFD + tHeader);
+	for (DWORD i = 0; i < entries->numberOfEntries; ++i) {
+		long data_address;
 		if (tHeader->brand == INTEL) {
-			VerifyInIntelMode(entry);
+			VerifyInIntelMode(entries->entries[i], tHeader);
 		}
 		else {
-			VerifyInMMMode(entry);
-		}
-		entry += sizeof(PENTRIES);
+			VerifyInMMMode(entries->entries[i], tHeader);
+		}		
 	}
 	CloseHandle(baseView);
 }
 
 LPVOID MappingHandler(HANDLE hFile) {
-	HANDLE hMap = CreateFileMapping(hFile, 0, PAGE_READONLY, 0, 0, 0);
+	HANDLE hMap = CreateFileMapping(hFile, 0, PAGE_READONLY | SEC_IMAGE, 0, 0, 0);
 	if (hMap == NULL) {
 		CloseHandle(hFile);
 		return NULL;
@@ -100,7 +162,7 @@ LPVOID MappingHandler(HANDLE hFile) {
 }
 
 LPVOID CreateFileHandlerA(CHAR *filename) {
-	HANDLE hFile = CreateFileA(filename, GENERIC_READ, 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+	HANDLE hFile = CreateFileA((LPCSTR)filename, GENERIC_READ , 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
 	if (hFile == INVALID_HANDLE_VALUE) {
 		return NULL;
 	}
@@ -108,7 +170,7 @@ LPVOID CreateFileHandlerA(CHAR *filename) {
 }
 
 LPVOID CreateFileHandlerW(WCHAR *filename) {
-	HANDLE hFile = CreateFileW(filename, GENERIC_READ, 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+	HANDLE hFile = CreateFileW((LPCWSTR)filename, GENERIC_READ , 0, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
 	if (hFile == INVALID_HANDLE_VALUE) {
 		return NULL;
 	}
--- ex5/ex5/Exercicio5.h
+++ ex5/ex5/Exercicio5.h
@@ -1,3 +1,24 @@
 #pragma once
+#include <Windows.h>
+#ifdef UNICODE 
+#define PrintExifTags PrintExifTagsW
 
-#ifdef 
+#else 
+#define PrintExifTags PrintExifTagsA
+#endif
+
+#ifdef EXERCICIO5_DLL_EXPORT
+	#define EXERCICIO5_DLL_API _declspec(dllexport)
+#else 
+#define EXERCICIO5_DLL_API _declspec(dllimport)
+#endif 
+
+#ifdef __cplusplus
+
+extern "C" {
+#endif 
+	EXERCICIO5_DLL_API VOID PrintExifTagsW(WCHAR * filename);
+	EXERCICIO5_DLL_API VOID PrintExifTagsA(CHAR* filename);
+#ifdef __cplusplus
+	}
+#endif 
