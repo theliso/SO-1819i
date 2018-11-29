25eb7ac502c7e9e314e535e71e2941fce75724a2
Theliso <luis.abrunhosa31@gmail.com>
2018-11-29 09:03:06 +0000

    adds tak exercise

--- 2_serie/Parte2/ProcA/Ex3/Parent.cpp
+++ 2_serie/Parte2/ProcA/Ex3/Parent.cpp
@@ -4,38 +4,20 @@
 #include <stdio.h>
 
 
-VOID createReadObserver(HANDLE readObserver) {
+#define BUFFER_SIZE 100
+
+VOID createReadObserver(LPVOID readObserver) {
 	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	LPVOID buffer = NULL;
-	DWORD bufferInfoRead = 0;
+	char buffer[BUFFER_SIZE];
+	DWORD written;
 	while (TRUE) {
-		if (ReadFile(readObserver, buffer, bufferInfoRead, NULL, NULL)) {
-			WriteFile(stdOut, buffer, bufferInfoRead, NULL, NULL);
+		if (ReadFile((HANDLE)readObserver, buffer, BUFFER_SIZE, &written, NULL)) {
+			WriteFile(stdOut, buffer, written, NULL, NULL);
 		}
 	}
 }
 
-VOID createWriteObserver(HANDLE writeObserver) {
-	HANDLE stdIn = GetStdHandle(STD_INPUT_HANDLE);
-	LPVOID buffer = NULL;
-	DWORD bufferInfoRead = 0;
-	while (TRUE) {
-		BOOL xpto = ReadFile(stdIn, buffer, bufferInfoRead, NULL, NULL);
-		if (xpto == 1) {
-			WriteFile(writeObserver, buffer, bufferInfoRead, NULL, NULL);
-		}
-	}
-}
-
-
-VOID associateOperationsToThreads(HANDLE write, HANDLE read, HANDLE *threadsObserver) {
-	HANDLE readObserver = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)createReadObserver, (LPVOID)read, 0, NULL);
-	HANDLE writeObserver = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)createWriteObserver, (LPVOID)write, 0, NULL);
-	threadsObserver[0] = readObserver;
-	threadsObserver[1] = writeObserver;
-}
-
-VOID createTalkInfraStructure() {
+VOID CreatePipes(HANDLE *pipes) {
 	SECURITY_ATTRIBUTES sa = { sizeof(sa) };
 	sa.bInheritHandle = TRUE;
 	HANDLE writeParent, readParent;
@@ -44,32 +26,64 @@ VOID createTalkInfraStructure() {
 	assert(parentPipe == TRUE);
 	BOOL childPipe = CreatePipe(&readParent, &writeChild, &sa, 0);
 	assert(childPipe == TRUE);
-	HANDLE threads[2];
-	associateOperationsToThreads(writeChild, readChild, threads);
-	HANDLE commandLine[3] = { writeParent, readParent, GetCurrentProcess() };
-	STARTUPINFO si = {sizeof(si)};
-	PROCESS_INFORMATION pi;
-	BOOL pChild = CreateProcess(_T("..\\Debug\\Ex3Child.exe"), (LPSTR)commandLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
-	assert(pChild == TRUE);
-	CloseHandle(readParent);
-	CloseHandle(writeParent);
-	ResumeThread(pi.hThread);
-	//test
-	while (TRUE) {
-		TCHAR buf[100];
-		_getts_s(buf, _countof(buf));
-	}
-	CloseHandle(writeChild);
-	CloseHandle(readChild);
-	CloseHandle(pi.hThread);
-	for (UINT i = 0; i < 2; ++i) {
-		CloseHandle(threads[i]);
-	}
+	pipes[0] = writeParent;
+	pipes[1] = readParent;
+	pipes[2] = writeChild;
+	pipes[3] = readChild;
 }
 
 
+HANDLE createChildProcess(HANDLE writeChild, HANDLE readChild) {
+	char cmd[128];
+	_stprintf_s(cmd, _T("..\\Debug\\Ex3Child.exe %d %d"), (DWORD)writeChild, (DWORD)readChild);
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+	BOOL pChild = CreateProcess(_T("..\\Debug\\Ex3Child.exe"), cmd, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+	assert(pChild == TRUE);
+	return pi.hProcess;
+}
+
+
+VOID ReadFromConsole(HANDLE stdIn, HANDLE write, char *buffer) {
+	DWORD written;
+	if (!ReadFile(stdIn, buffer, BUFFER_SIZE, &written, NULL)) {
+		printf("couldn't read");
+		return;
+	}
+	buffer[written] = '\0';
+	WriteFile(write, buffer, strlen(buffer), NULL, NULL);
+}
+
+VOID WriteToHandle(HANDLE read, char *buffer) {
+	DWORD written;
+	WriteFile(read, buffer, strlen(buffer), &written, NULL);
+}
+
+VOID ReadConsole(HANDLE write, HANDLE read) {
+	HANDLE threadRead = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)createReadObserver, (LPVOID)read, 0, NULL);
+	while (TRUE) {
+		char buffer[BUFFER_SIZE];
+		ReadFromConsole(GetStdHandle(STD_INPUT_HANDLE), write, buffer);
+		if (buffer != NULL) {
+			WriteToHandle(read, buffer);
+		}
+	}
+	CloseHandle(threadRead);
+	CloseHandle(write);
+	CloseHandle(read);
+}
+
+VOID Talk(HANDLE write, HANDLE read, HANDLE pChild) {
+	ReadConsole(write, read);
+	WaitForSingleObject(pChild, INFINITE);	
+}
 
 int main() {
-	createTalkInfraStructure();
+	HANDLE pipes[4];
+	CreatePipes(pipes);
+	HANDLE pChild = createChildProcess(pipes[2], pipes[3]);
+	CloseHandle(pipes[2]);
+	CloseHandle(pipes[3]);
+	Talk(pipes[0], pipes[1], pChild);
 	return 0;
 }
--- 2_serie/Parte2/ProcA/Ex3Child/Child.cpp
+++ 2_serie/Parte2/ProcA/Ex3Child/Child.cpp
@@ -3,53 +3,55 @@
 #include <tchar.h>
 #include <stdio.h>
 
-VOID createReadObserver(HANDLE readObserver) {
+#define BUFFER_SIZE 200
+
+VOID createReadObserver(LPVOID readObserver) {
 	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	LPVOID buffer = NULL;
-	DWORD bufferInfoRead = 0;
+	char buffer[BUFFER_SIZE];
+	DWORD written;
 	while (TRUE) {
-		if (ReadFile(readObserver, buffer, bufferInfoRead, NULL, NULL)) {
-			WriteFile(stdOut, buffer, bufferInfoRead, NULL, NULL);
-		}
-	}
-}
-
-VOID createWriteObserver(HANDLE writeObserver) {
-	HANDLE stdIn = GetStdHandle(STD_INPUT_HANDLE);
-	LPVOID buffer = NULL;
-	DWORD bufferInfoRead = 0;
-	while (TRUE) {
-		if (ReadFile(stdIn, buffer, bufferInfoRead, NULL, NULL)) {
-			WriteFile(writeObserver, buffer, bufferInfoRead, NULL, NULL);
+		if (ReadFile((HANDLE)readObserver, buffer, BUFFER_SIZE, &written, NULL)) {
+			WriteFile(stdOut, buffer, written, NULL, NULL);
 		}
 	}
 }
 
 
-VOID createOperationsToEvents(HANDLE write, HANDLE read, HANDLE *threads) {
-	HANDLE readObserver = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)createReadObserver, (LPVOID)read, CREATE_SUSPENDED, NULL);
-	HANDLE writeObserver = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)createReadObserver, (LPVOID)write, CREATE_SUSPENDED, NULL);
-	threads[0] = readObserver;
-	threads[1] = writeObserver;
+VOID ReadFromConsole(HANDLE hInput, HANDLE hWrite, char *buffer) {
+
+	DWORD written;
+
+	if (!ReadFile(hInput, buffer, BUFFER_SIZE, &written, NULL)) {
+		printf("error on reading input");
+		return;
+	}
+	buffer[written] = '\0';
+	WriteFile(hWrite, buffer, strlen(buffer), NULL, NULL);
+}
+
+VOID WriteToHandle(HANDLE read, char *buffer) {
+	DWORD written;
+	WriteFile(read, buffer, strlen(buffer), &written, NULL);
+}
+
+VOID ReadConsole(HANDLE write, HANDLE read) {
+	HANDLE readObserver = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)createReadObserver, (LPVOID)read, NULL, NULL);
+	while (TRUE) {
+		char buffer[BUFFER_SIZE];
+		ReadFromConsole(GetStdHandle(STD_INPUT_HANDLE), write, buffer);
+		if (buffer != NULL) {
+			WriteToHandle(read, buffer);
+		}
+	}
+	CloseHandle(write);
+	CloseHandle(read);
+	CloseHandle(readObserver);
 }
 
 
 int main(int argc, char *argv[]) {
-	HANDLE writeParent = argv[0];
-	HANDLE readParent = argv[1];
-	HANDLE parentProcess = argv[2];
-	HANDLE threads[2];
-	createOperationsToEvents(writeParent, readParent, threads);
-	ResumeThread(threads[0]);
-	ResumeThread(threads[1]);
-	while (TRUE) {
-		TCHAR buf[100];
-		_getts_s(buf, _countof(buf));
-	}
-	CloseHandle(writeParent);
-	CloseHandle(readParent);
-	for (UINT i = 0; i < 2; ++i) {
-		CloseHandle(threads[i]);
-	}
+	HANDLE writeChild = (HANDLE)_tstoi(argv[1]);
+	HANDLE readChild = (HANDLE)_tstoi(argv[2]);
+	ReadConsole(writeChild, readChild);
 	return 0;
 }
